185a4e998df195f9e8b59aa7341bf267ccd767e0
Juan Pablo Morales <juanpablom6789@gmail.com>
2025-09-08 23:46:27 -0300

    added endpoint to send template

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 import json
 from datetime import datetime
+import httpx
 from fastapi import FastAPI, status, Request, Query, HTTPException
 from fastapi.responses import PlainTextResponse
 from pydantic import BaseModel
@@ -32,3 +33,33 @@ async def receive_webhook(request: Request):
     print(f"\n\nWebhook received {timestamp}\n")
     print(json.dumps(body, indent=2))
     return {"status": "ok"}
+
+
+@app.post("/send_template")
+async def send_template(to: str):
+    url = f"https://graph.facebook.com/v22.0/{meta_settings.WHATSAPP_PHONE_NUMBER_ID}/messages"
+    headers = {
+        "Authorization": f"Bearer {meta_settings.WHATSAPP_ACCESS_TOKEN}",
+        "Content-Type": "application/json",
+    }
+
+    payload = {
+        "messaging_product": "whatsapp",
+        "to": to,
+        "type": "template",
+        "template": {
+            "name": "primeros_pasos",
+            "language": {"code": "es_AR"},
+        },
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, headers=headers, json=payload)
+
+    if response.status_code == 200:
+        return {"status": "Message sent successfully"}
+    else:
+        raise HTTPException(
+            status_code=response.status_code,
+            detail=f"Failed to send message: {response.text}",
+        )
