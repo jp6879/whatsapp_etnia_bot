4000b6641b43fd192ca8b910afafdb2882ddade8
Juan Pablo Morales <juanpablom6789@gmail.com>
2025-09-08 21:30:54 -0300

    Updated app

--- app/app.py
+++ app/app.py
@@ -1,21 +1,34 @@
-from fastapi import FastAPI, status
+import json
+from datetime import datetime
+from fastapi import FastAPI, status, Request, Query, HTTPException
+from fastapi.responses import PlainTextResponse
 from pydantic import BaseModel
 from .config import meta_settings
 
 app = FastAPI()
 
 
-@app.webhooks.get("/recieve")
-def verify_token(mode: str, challenge: str, token: str):
-    if mode == "subscribe" and token == meta_settings.WHATSAPP_VERIFY_TOKEN:
+@app.get("/verify", response_class=PlainTextResponse, name="Verify Webhook")
+async def verify_webhook(
+    hub_mode: str = Query(..., alias="hub.mode"),
+    hub_challenge: str = Query(..., alias="hub.challenge"),
+    hub_verify_token: str = Query(..., alias="hub.verify_token"),
+):
+    if (
+        hub_mode == "subscribe"
+        and hub_verify_token == meta_settings.WHATSAPP_VERIFY_TOKEN
+    ):
         print("WEBHOOK_VERIFIED")
-        return status.HTTP_200_OK, challenge
+        return hub_challenge
 
     print("WEBHOOK_VERIFICATION_FAILED")
     return status.HTTP_403_FORBIDDEN
 
 
-@app.webhooks.post("/send")
-def send_message(payload: BaseModel):
-    print(payload)
-    return status.HTTP_200_OK
+@app.post("/webhook", name="receive_webhook")
+async def receive_webhook(request: Request):
+    body = await request.json()
+    timestamp = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"\n\nWebhook received {timestamp}\n")
+    print(json.dumps(body, indent=2))
+    return {"status": "ok"}
