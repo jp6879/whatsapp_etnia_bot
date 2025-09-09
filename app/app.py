d4c8f594fa7673f0dbae13c7306fbeb73ee4e962
Juan Pablo Morales <juanpablom6789@gmail.com>
2025-09-09 19:21:10 -0300

    Updates on recieve webhook msg

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 import json
 from datetime import datetime
 import httpx
-from fastapi import FastAPI, status, Request, Query, HTTPException
+from fastapi import FastAPI, requests, status, Request, Query, HTTPException
 from fastapi.responses import PlainTextResponse
 from pydantic import BaseModel
 from .config import meta_settings
@@ -26,40 +26,32 @@ async def verify_webhook(
     return status.HTTP_403_FORBIDDEN
 
 
-@app.post("/webhook", name="receive_webhook")
-async def receive_webhook(request: Request):
-    body = await request.json()
-    timestamp = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"\n\nWebhook received {timestamp}\n")
-    print(json.dumps(body, indent=2))
-    return {"status": "ok"}
+@app.post("/webhook")
+async def response_webhook(request: Request):
+    data = await request.json()
+    try:
+        message = data["entry"][0]["changes"][0]["value"]["messages"][0]
+        sender = message["from"]
+        text = message["text"]["body"]
 
+        # Simple rule-based bot
+        if "hello" in text.lower():
+            reply = "Hi 👋! How can I help you today?"
+        else:
+            reply = "Sorry, I didn’t understand. Please try again."
 
-@app.post("/send_template")
-async def send_template(to: str):
-    url = f"https://graph.facebook.com/v22.0/{meta_settings.WHATSAPP_PHONE_NUMBER_ID}/messages"
-    headers = {
-        "Authorization": f"Bearer {meta_settings.WHATSAPP_ACCESS_TOKEN}",
-        "Content-Type": "application/json",
-    }
-
-    payload = {
-        "messaging_product": "whatsapp",
-        "to": to,
-        "type": "template",
-        "template": {
-            "name": "primeros_pasos",
-            "language": {"code": "es_AR"},
-        },
-    }
-
-    async with httpx.AsyncClient() as client:
-        response = await client.post(url, headers=headers, json=payload)
-
-    if response.status_code == 200:
-        return {"status": "Message sent successfully"}
-    else:
-        raise HTTPException(
-            status_code=response.status_code,
-            detail=f"Failed to send message: {response.text}",
-        )
+        url = f"https://graph.facebook.com/v21.0/{meta_settings.WHATSAPP_PHONE_NUMBER_ID}/messages"
+        headers = {
+            "Authorization": f"Bearer {meta_settings.WHATSAPP_ACCESS_TOKEN}",
+            "Content-Type": "application/json",
+        }
+        payload = {
+            "messaging_product": "whatsapp",
+            "to": sender,
+            "type": "text",
+            "text": {"body": reply},
+        }
+        requests.post(url, headers=headers, json=payload)
+    except Exception as e:
+        print("Webhook error:", e)
+    return "OK"
